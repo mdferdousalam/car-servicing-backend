40198e16875d7ffc308fb4b346c58ec4974b40f1
mdferdousalam <mdferdousalam1989@yahoo.com>
2024-08-12 11:02:53 +0600

    request validation in login done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,52 +1,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import { ERROR, SUCCESS } from "../shared/api.response.types";
-import { registerSchema, loginSchema } from "./user.schema";
 import { UserServices } from "./user.service";
-import { IUser } from "./user.interface";
 import httpStatus from "http-status";
 
 const registerUser = async (req: Request, res: Response) => {
-  const validationResult = registerSchema.safeParse(req.body);
-  if (!validationResult.success) {
+  try {
+    const user = await UserServices.registerUserIntoDB(req.body);
+    return SUCCESS(
+      res,
+      httpStatus.CREATED,
+      "User Registered Successfully",
+      user
+    );
+  } catch (error: any) {
     return ERROR(
       res,
-      400,
-      "Failed to validate User registration data, please provide correct data",
-      [validationResult.error.errors],
+      httpStatus.INTERNAL_SERVER_ERROR,
+      "Failed to register User",
+      [error.message]
     );
-  }
-
-  try {
-    const user = await UserServices.registerUserIntoDB(
-      validationResult.data as unknown as IUser
-    );
-    return SUCCESS(res,httpStatus.CREATED, "User Registered Successfully", user);
-  } catch (error: any) {
-    return ERROR(res, httpStatus.INTERNAL_SERVER_ERROR, "Failed to register User", [error.message]);
   }
 };
 
 const loginUser = async (req: Request, res: Response) => {
-  const validationResult = loginSchema.safeParse(req.body);
-  if (!validationResult.success) {
-    return ERROR(
-      res,
-      httpStatus.NOT_FOUND,
-      "Failed to validate User login data, please provide correct data",
-      [validationResult.error.errors],
-      
-    );
-  }
-
   try {
-    const user = await UserServices.loginUserFromDB(validationResult.data);
+    const user = await UserServices.loginUserFromDB(req.body);
     return SUCCESS(res, httpStatus.OK, "User Logged in Successfully", user);
   } catch (error: any) {
-    return ERROR(res, httpStatus.INTERNAL_SERVER_ERROR, "Failed to log in User", [error.message]);
+    return ERROR(
+      res,
+      httpStatus.INTERNAL_SERVER_ERROR,
+      "Failed to log in User",
+      [error.message]
+    );
   }
 };
-
 
 export const UserControllers = {
   registerUser,
